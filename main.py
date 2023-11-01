0ed8b5e459e34f058d8ea661be0626f1cc4d12c8
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-11-01 17:22:55 -0300

    Feat: Added different types of times to countdown

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
+WORK_MIN = 1
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
@@ -16,7 +17,21 @@ LONG_BREAK_MIN = 20
 
 
 def start_timer():
-    count_down(5 * 60)
+    global reps
+    reps += 1
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 9:
+        count_down(long_break_sec)
+        title_label.config(text="Break", fg=RED)
+    elif reps % 2 == 0:
+        title_label.config(text="Break", fg=PINK)
+        count_down(short_break_sec)
+    else:
+        title_label.config(text="Work", fg=GREEN)
+        count_down(work_sec)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
@@ -32,7 +47,8 @@ def count_down(count):
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000, count_down, count - 1)
-
+    else:
+        start_timer()
 
 # ---------------------------- UI SETUP ------------------------------- #
 
